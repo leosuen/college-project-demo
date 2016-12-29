c4bcc865751b575dd512124e8e533e8b2603dc36
leosuen <computer.king.suen@gmail.com>
2016-12-29 15:41:17 +0800

    update 1.022

--- questionphp/sendingHandler.php
+++ questionphp/sendingHandler.php
@@ -12,7 +12,7 @@
     $question_when = $obj['question_when'];
     $question_text = $obj['question_text'];
     
-    if(isset($question_why) && isset($question_who) && isset($question_what) && isset($question_where) && isset($question_when)){
+    if(isset($question_when)){
         $sql_question_schema = "INSERT INTO `triz_question`(`ID`, `Name`, `Emp_ID`, `Date`, `quest_why`, `quest_who`, `quest_what`, `quest_where`, `quest_when`, `note`) VALUES (NULL,'$name','$id','$date','$question_why','$question_who','$question_what','$question_where','$question_when','$question_text')";
         $stmt = $conn->prepare($sql_question_schema);
         $stmt->execute();
