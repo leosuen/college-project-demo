168c18b73d778d73a1433f00cf96ce1f46a53a8f
leosuen <computer.king.suen@gmail.com>
2017-01-02 21:54:28 +0800

    beta 1.026

--- js/custom.js
+++ js/custom.js
@@ -112,37 +112,8 @@
   	  return false;
 		}
   });
-
-	// Loading
-	//----------------------------------------------
-  /*function remove_loading($form)
-  {
-  	$form.find('[type=submit]').removeClass('error success');
-  	$form.find('.login-form-main-message').removeClass('show error success').html('');
-  }
-
-  function form_loading($form)
-  {
-    $form.find('[type=submit]').addClass('clicked').html(options['btn-loading']);
-  }
-  
-  function form_success($form)
-  {
-	  $form.find('[type=submit]').addClass('success').html(options['btn-success']);
-	  $form.find('.login-form-main-message').addClass('show success').html(options['msg-success']);
-  }
-
-  function form_failed($form)
-  {
-  	$form.find('[type=submit]').addClass('error').html(options['btn-error']);
-  	$form.find('.login-form-main-message').addClass('show error').html(options['msg-error']);
-  }*/
-
-	// Dummy Submit Form (Remove this)
-	//----------------------------------------------
-	// This is just a dummy form submission. You should use your AJAX function or remove this function if you are not using AJAX.
   /* login submit */
-  function submitForm($form)
+  function submitForm1($form)
   {  
     var data = $("#login-form").serialize();
   
